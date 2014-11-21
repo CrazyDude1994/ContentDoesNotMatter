6836e64430e45afb9f21e95c7bafb7d4ec638e23
Sergey Kartavtsev <crazyserega1994@mail.ru>
2014-11-21 23:28:33 +0200

    Added state save on orientation change in MainActivity

--- src/com/crazy/contentdoesnotmatter/fragments/MainFragment.java
+++ src/com/crazy/contentdoesnotmatter/fragments/MainFragment.java
@@ -1,16 +1,19 @@
 package com.crazy.contentdoesnotmatter.fragments;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.crazy.contentdoesnotmatter.R;
-import com.crazy.contentdoesnotmatter.activities.MainActivity;
 import com.crazy.contentdoesnotmatter.views.PhotoView;
 
 public class MainFragment extends Fragment {
+	
+	private static final String TOP_IMAGE = "EXTRA_TOP_IMAGE";
+	private static final String BOT_IMAGE = "EXTRA_BOT_IMAGE";
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,13 +22,23 @@ public class MainFragment extends Fragment {
 				false);
 		((PhotoView)rootView.findViewById(R.id.topImage)).setRotate(-5);
 		((PhotoView)rootView.findViewById(R.id.bottomImage)).setRotate(5);
+		
+		if (savedInstanceState != null) {
+			Bitmap topImage = savedInstanceState.getParcelable(TOP_IMAGE);
+			Bitmap botImage = savedInstanceState.getParcelable(BOT_IMAGE);
+			
+			if (topImage != null)
+				((PhotoView)rootView.findViewById(R.id.topImage)).setImage(topImage);
+			if (botImage != null)
+				((PhotoView)rootView.findViewById(R.id.bottomImage)).setImage(botImage);
+		}
 		return rootView;
 	}
 	
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
-		outState.putParcelable(MainActivity.TOP_IMAGE, ((PhotoView)getView().findViewById(R.id.topImage)).getImage());
-		outState.putParcelable(MainActivity.BOT_IMAGE, ((PhotoView)getView().findViewById(R.id.topImage)).getImage());
+		outState.putParcelable(TOP_IMAGE, ((PhotoView)getView().findViewById(R.id.topImage)).getImage());
+		outState.putParcelable(BOT_IMAGE, ((PhotoView)getView().findViewById(R.id.bottomImage)).getImage());
 		super.onSaveInstanceState(outState);
 	}
 }
